59bc8160827368f3f7a7683833a28e3c16087031
support <support@WEMA-TRAINING-S>
2022-08-16 10:54:52 -0700

    Add project files.

---
+++ DelegateCreation/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateCreation{
+
+
+
+
+
+
+    /// <summary>    /// this is a code to show the progress when transferring files    /// </summary>                                                                                                             internal class Monitor    {
+        public delegate void TransferLeft(int transferLeft);
+
+
+
+
+
+
+
+        /// <summary>        /// I had to put a delegate to see how much byte is left when a user is transferring a file        /// </summary>
+        public static void TransferFile(TransferLeft tl)        {            for (int i = 0; i < 20000; i++)            {                var markedTransfer = i.ToString();                tl(i);            }        }    }}
---
+++ DelegateCreation/Program.cs
@@ -0,0 +1,3 @@
+﻿namespace DelegateCreation{    internal class Program    {        static void Main(string[] args)        {
+            Monitor.TransferFile(ProgressMeter);        }        public static void ProgressMeter(int transferredBytes)        {
+            Console.WriteLine($"the progress report {transferredBytes} remaining");        }    }}
